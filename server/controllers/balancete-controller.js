b12d50822169be7f00674be56808f816fc5bcd52
ahsouza <annibalhsouza@gmail.com>
2021-01-23 12:51:46 -0300

    first commit

---
+++ server/controllers/balancete-controller.js
@@ -0,0 +1,79 @@
+// import módulos
+const IBMCloudEnv = require('ibm-cloud-env')
+IBMCloudEnv.init('/server/config/mappings.json')
+// Configurando Cloudant
+const CloudantSDK = require('@cloudant/cloudant');
+const cloudant = new CloudantSDK(IBMCloudEnv.getString('cloudant_url'))
+
+// criando banco de dados nomeado 'balancete' caso ele ainda não existe
+cloudant.db.create('balancete')
+  .then(data => {
+    console.log('Banco de Dados "balancete" criado com sucesso!')
+  })
+  .catch(error => {
+    if (error.error === 'file_exists') {
+      console.log('Banco de Dados "balancete" já existe!')
+    } else {
+      console.log('Ocorreu um erro ao criar Banco De Dados "balancete"', error.error)
+    }
+  })
+const balancete = cloudant.db.use('balancete');
+
+// get names from database
+exports.getBalancetes = (req, res, next) => {
+  console.log('In route - getBalancetes')
+
+  return balancete.list({include_docs: true})
+    .then(fetchedBalancete => {
+      let balancetes = [];
+      let row = 0;
+      fetchedBalancete.rows.forEach(fetchedBalancete => {
+        balancetes[row] = {
+          _id: fetchedBalancete.id,
+          companyId: fetchedBalancete.doc.companyId,
+          balanceSheet: fetchedBalancete.doc.balanceSheet,
+          date: fetchedBalancete.doc.date,
+          timestamp: fetchedName.doc.timestamp,
+        };
+        row = row + 1;
+      });
+      console.log('Balancetes recebido com sucesso!')
+      return res.status(200).json(balancetes)
+    })
+    .catch(error => {
+      console.log('Falha ao receber balancetes')
+      return res.status(500).json({
+        message: 'Falha ao tentar pegar balancete.',
+        error: error,
+      })
+    })
+}
+
+// add name to database
+exports.addBalancete = (req, res, next) => {
+  console.log('In route - addBalancete');
+  let balancete = {
+    companyId: req.body.companyId,
+    balanceSheet: req.body.balanceSheet,
+    date: req.body.date,
+    timestamp: req.body.timestamp,
+  };
+  return balancete.insert(balancete)
+    .then(addedBalancete => {
+      console.log('Balancete adicionado com sucesso!');
+      return res.status(201).json({
+        _id: addedBalancete.id,
+        companyId: addedBalancete.companyId,
+        balanceSheet: addedBalancete.balanceSheet,
+        date: addedBalancete.date,
+        timestamp: addedBalancete.timestamp,
+      });
+    })
+    .catch(error => {
+      console.log('Falha ao tentar registrar balancete');
+      return res.status(500).json({
+        message: 'Falha ao tentar salvar balancete.',
+        error: error,
+      });
+    });
+};
---
+++ server/routes/balancete-route.js
@@ -0,0 +1,32 @@
+// import dependencies and initialize the express router
+const
+ express = require('express'),
+ { body, validationResult } = require('express-validator'),
+ BalancetesController = require('../controllers/balancete-controller')
+
+const router = express.Router();
+
+// standardized validation error response
+const validate = validations => {
+  return async(req, res, next) => {
+    await Promise.all(validations.map(validation => validation.run(req)))
+
+    const errors = validationResult(req)
+    if (errors.isEmpty()) {
+      return next();
+    }
+
+    res.status(400).json({errors: errors.array()})
+  }
+};
+
+// definindo rotas
+router.get('', BalancetesController.getBalancetes);
+router.post('', validate([
+  body('date').isInteger(),
+  body('balanceSheet').isAlphanumeric(),
+  body('companyId').isISO8601(),
+  body('timestamp').isISO8601(),
+]), BalancetesController.addBalancete);
+
+module.exports = router;
